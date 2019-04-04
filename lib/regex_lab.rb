772e8efd62eb0654a920ef154c77e0a84669e8c0
Ryan Mui <reachryanmui@gmail.com>
2019-04-04 03:56:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  a = text.scan(/^[A-Z]W$/)
+  a = text.scan(/^[A-Z]\W$/)
   binding.pry
 end
 
